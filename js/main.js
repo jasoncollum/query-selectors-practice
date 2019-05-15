ad8725fcb65745b7f0a2aa1cbe677f17e0f2b7bd
Jason Collum <jasoncollum@gmail.com>
2019-05-15 08:19:18 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log('Hello main.js');
+
+const sectionEl = document.querySelector('.article__section');
+
+console.log("sectionEL", sectionEl);
+
+const footerEl = document.querySelector('.article__footer');
+
+console.log("footerEl", footerEl);
+
+const smallTextEl = document.querySelector('.smallText');
+smallTextEl.classList.remove('smallText');
+smallTextEl.classList.add('largeText');
+
+const sectionAll = document.querySelectorAll('.article__section');
+console.log("sectionAll", sectionAll);
+console.log("first element", sectionAll[0]);
